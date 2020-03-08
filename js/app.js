c31f95d212aad56728a24adee62e97a305afa2a0
SergeChurkin-ca <sergechurkin@yahoo.com>
2020-03-07 20:32:03 -0500

    disappering/appering navbat, border onclick

--- js/app.js
+++ js/app.js
@@ -11,19 +11,51 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
+ */
 
+var sectionOne = document.getElementById('section1');
+var sectionTwo = document.getElementById('section2');
+var sectionThree = document.getElementById('section3');
 
+let sections = document.querySelectorAll('div > p');
+let i;
+for (i = 0; i < sections.length; i++) {
+    sections[i].style.backgroundColor = 'orange';
+}
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
+ */
+
+
+// Styling section on mouseover
+
+sections.addEventListener('mouseenter', function() {
+    sections.style = "border: outset 1px grey; border-radius: 1em; box-shadow: 2px 2px 2px black";
+});
+
+sections.addEventListener('mouseleave', function() {
+    sections.style = "none";
+});
+
+// Smooth roolin - roolout nav
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector("nav").style.top = "0";
+    } else {
+        document.querySelector("nav").style.top = "-5em";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
 
 
 
@@ -31,7 +63,7 @@
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
 
 // build the nav
 
@@ -46,12 +78,19 @@
  * End Main Functions
  * Begin Events
  * 
-*/
+ */
 
 // Build menu 
 
 // Scroll to section on link click
 
+
+
 // Set sections as active
 
 
+
+
+
+
+// }
