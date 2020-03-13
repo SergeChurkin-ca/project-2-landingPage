3fc6f4c96accb132360340b3922061e3c2282e61
SergeChurkin-ca <sergechurkin@yahoo.com>
2020-03-12 23:13:47 -0400

    autosave

--- js/app.js
+++ js/app.js
@@ -19,17 +19,20 @@
  */
 
 
-const navItems = document.getElementById('navbar__list');
-
-const myTextInput = document.getElementById('myTextInput');
-
 const newPost = document.querySelector('[id=section4]');
 const comments = document.getElementById('section-4');
 comments.style = "display: none"
 
 const newSpanElem = document.createElement('p');
 
+const myTextInput = document.getElementById('myTextInput');
+const nameInput = document.getElementById('nameInput');
+
+const myPhotoInput = document.querySelector('#myPhotoInput').files[0].path;
+
 const myButton = document.getElementById('myButton');
+const buttonPhoto = document.getElementById('buttonPhoto');
+
 /** 
  * End Global Variables
  * Start Helper Functions
@@ -50,16 +53,29 @@ window.onscroll = function() {
 }
 
 
+// Posting comment
 const node = document.createTextNode(myTextInput);
 newSpanElem.appendChild(node);
+newSpanElem.appendChild(node);
+
 
 myButton.addEventListener("click", () => {
-    newPost.insertAdjacentHTML('beforeend', myTextInput.value);
+    newPost.insertAdjacentHTML('beforeend', myTextInput.value + '\n' + nameInput.value + '<br><br>');
     comments.style = "display: inline-block";
+    myTextInput.value = "";
+    nameInput.value = "";
 })
 
+// Posting picture on comment
 
-document.querySelector('p').addEventListener('mouseenter', () => {
+buttonPhoto.addEventListener("click", () => {
+    newPost.insertAdjacentHTML('beforeend', myPhotoInput + "<br><br>");
+    myPhotoInput.style = "height: 50%";
+});
+
+
+// hover decoration for articles
+document.querySelector('div').addEventListener('mouseenter', () => {
     let sections = document.querySelectorAll('div > p');
     for (i = 0; i < sections.length; i++) {
         sections[i].style = "border: outset 1px grey; border-radius: 1em; box-shadow: 1px 1px 1px black";
@@ -71,20 +87,6 @@ document.querySelector('div p').addEventListener('mouseleave', () => {
         sections[i].style = "none";
     }
 });
-
-
-
-
-// Styling section on mouseover
-
-sections.addEventListener('mouseenter', function() {
-    sections.style = "border: outset 1px grey; border-radius: 1em; box-shadow: 2px 2px 2px black";
-});
-
-sections.addEventListener('mouseleave', function() {
-    sections.style = "none";
-});
-
 
 
 
